9bf6dfe7b960cc811c97ad70b36f2b746c8cfc35
ashtalal <ashtalal@gmail.com>
2018-01-12 02:56:32 +0800

    php activity

---
+++ php activity/php-activity/activity.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
+	<title>PHP activities</title>
+
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+
+	<?php
+
+
+	// Activity 1
+
+	// phpinfo()
+
+
+	// Activity 2
+
+	// echo "Tomorrow is the new day!";
+
+	// Activity 3
+
+		$var = 'PHP Tutorial';
+
+		echo "<h3> $var </h3> 
+			<p> PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.</p> <br>
+
+			Go to the $var.";
+
+	// Activity 4
+
+			
+	?>
+
+	
+
+</body>
+</html>
